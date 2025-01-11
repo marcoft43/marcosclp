81c6c0500ebd7088776f4a1650f129cfd06a91a8
Yucong Ma <marco.clp42@gmail.com>
2025-01-11 01:30:46 +0100

    Added first picture

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Stack } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h4>I try to take some pictures.</h4>
+        <h4>I see that it is by no means useless to travel, if a man wants to see something new.</h4>
       </header>
-      <stack>
-        <div className='Orange-Dynamite'>Catania</div>
-        <div className='Deep-Cyan'>Prague</div>
-      </stack>
+      
+      <div className='container'>
+        <div className='centered-div'>
+          <img src="./Catania_Etnea.jpeg" alt="Catania Etnea" style={{width: '60%', height: 'auto'}}/>
+          <div className="text-with-bar">
+            <div className="color-bar-orange-dynamite"></div>
+            <span className="text">Catania - Via Etnea</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
