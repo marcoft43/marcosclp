16e85d039ed96cccb32748bfaece0e8578f1d9c7
Yucong Ma <marco.clp42@gmail.com>
2025-01-11 21:56:32 +0100

    Resized pictures for munich

--- src/pages/Munich.js
+++ src/pages/Munich.js
@@ -16,15 +16,15 @@ const Munich = () => {
           </div>
         </div>
         <div className='centered-div'>
-          <img src={MunichStarnbergsee} alt="Munich Starnbergsee" style={{width: '60%', height: 'auto'}}/>
-          <div className="text-with-bar padding-left-20">
+          <img src={MunichStarnbergsee} alt="Munich Starnbergsee" style={{width: '90%', height: 'auto'}}/>
+          <div className="text-with-bar padding-left-5">
             <div className="color-bar bice-blue"></div>
             <span className="text">München - Starnberger See</span>
           </div>
         </div>
         <div className='centered-div'>
-          <img src={MunichMarienPlatz} alt="Munich Marien Platz" style={{width: '60%', height: 'auto'}}/>
-          <div className="text-with-bar padding-left-20">
+          <img src={MunichMarienPlatz} alt="Munich Marien Platz" style={{width: '90%', height: 'auto'}}/>
+          <div className="text-with-bar padding-left-5">
             <div className="color-bar licorice"></div>
             <span className="text">München - Marienplatz</span>
           </div>
