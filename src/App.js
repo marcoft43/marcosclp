3c1d74c7557e8d63a4ae98dadb00054e36f502c2
Yucong Ma <marco.clp42@gmail.com>
2025-01-11 03:00:43 +0100

    updated font

--- src/App.js
+++ src/App.js
@@ -2,30 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import { Stack } from 'react-bootstrap';
 
+import CataniaEtnea from './pictures/Catania_Etnea.jpeg';
+import CataniaStesicoro from './pictures/Catania_Stesicoro.jpeg';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h4>Eine Besondere Vergänglichkeit</h4>
+        <h2>Eine Besondere Vergänglichkeit</h2>
       </header>
       
       <div className='container'>
         <div className='centered-div'>
-          <img src="./Catania_Etnea.jpeg" alt="Catania Etnea" style={{width: '60%', height: 'auto'}}/>
+          <img src={CataniaEtnea} alt="Catania Etnea" style={{width: '60%', height: 'auto'}}/>
           <div className="text-with-bar-vertical">
             <div className="color-bar-orange-dynamite"></div>
             <span className="text">Catania - Via Etnea</span>
           </div>
         </div>
         <div className='centered-div'>
-          <img src="./Catania_Stesicoro.jpeg" alt="Catania Stesicoro" style={{width: '90%', height: 'auto'}}/>
+          <img src={CataniaStesicoro} alt="Catania Stesicoro" style={{width: '90%', height: 'auto'}}/>
           <div className="text-with-bar-horizontal">
             <div className="color-bar-klein-blue"></div>
             <span className="text">Catania - Piazza Stesicoro</span>
           </div>
         </div>
       </div>
-      
+
     </div>
   );
 }
