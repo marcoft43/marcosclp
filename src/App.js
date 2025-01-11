e396aaa0aab05b0f8fde240134c8800ee42a46d5
Yucong Ma <marco.clp42@gmail.com>
2025-01-11 01:41:10 +0100

    Added second picture

--- src/App.js
+++ src/App.js
@@ -12,9 +12,16 @@ function App() {
       <div className='container'>
         <div className='centered-div'>
           <img src="./Catania_Etnea.jpeg" alt="Catania Etnea" style={{width: '60%', height: 'auto'}}/>
-          <div className="text-with-bar">
+          <div className="text-with-bar-vertical">
             <div className="color-bar-orange-dynamite"></div>
             <span className="text">Catania - Via Etnea</span>
+          </div>
+        </div>
+        <div className='centered-div'>
+          <img src="./Catania_Stesicoro.jpeg" alt="Catania Stesicoro" style={{width: '90%', height: 'auto'}}/>
+          <div className="text-with-bar-horizontal">
+            <div className="color-bar-klein-blue"></div>
+            <span className="text">Catania - Piazza Stesicoro</span>
           </div>
         </div>
       </div>
