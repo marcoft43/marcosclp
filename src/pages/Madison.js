3744bcede3a28ea8c1ea8ccf246211a7b3920e2a
Yucong Ma <marco.clp42@gmail.com>
2025-01-13 10:33:58 +0100

    Changed link color scheme

--- src/pages/Madison.js
+++ src/pages/Madison.js
@@ -11,14 +11,14 @@ const Madison = () => {
           <img src={Madison1} alt="Madison" style={{width: '60%', height: 'auto'}}/>
           <div className="text-with-bar padding-left-20">
             <div className="color-bar osu-scarlet"></div>
-            <span className="text">Madison - UW Madison Campus</span>
+            <span className="text">Madison, WI - UW Madison Campus</span>
           </div>
         </div>
         <div className='centered-div'>
           <img src={Madison2} alt="Madison" style={{width: '60%', height: 'auto'}}/>
           <div className="text-with-bar padding-left-20">
             <div className="color-bar dark-pastel-orange"></div>
-            <span className="text">Madison - Wisconsin State Capitol</span>
+            <span className="text">Madison, WI - Wisconsin State Capitol</span>
           </div>
         </div>
       </div>
